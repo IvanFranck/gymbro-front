6dc756696f09e3a00874a111057c4d7ae306ed87
NZIMA Ivan <nzimaivan007@gmail.com>
2025-04-18 16:03:03 +0100

    Enhance offers, pricing, and services pages: add search functionality, modal components for creating new entries, and improve data fetching logic.

--- pages/offers/pricing.vue
+++ pages/offers/pricing.vue
@@ -1,5 +1,35 @@
 <template>
-    <div>
-        offres pricing
+    <div class="w-full flex justify-end">
+      <UButton color="primary" class="cursor-pointer" @click="toggleModal">Nouvelle offre</UButton>
     </div>
-</template>
+    <div class="mt-4">
+      <DatatablePriceList v-model:is-modal-open="open" />
+    </div>
+    <UModal @close="handleCloseModal" v-model:open="open">
+      <template #content>
+        <FormPriceList @close="closeModal" />
+      </template>
+    </UModal>
+  </template>
+  
+  <script lang="ts" setup>
+  import { DatatablePriceList, FormPriceList, UButton } from '#components';
+  
+  const open = ref(false);
+  const priceListStore = usePriceListStore();
+  
+  function toggleModal() {
+    open.value = !open.value
+    priceListStore.setPricingItemEdit(null);
+  }
+  
+  function closeModal(){
+    open.value = false;
+  }
+  
+  function handleCloseModal() {
+    priceListStore.setPricingItemEdit(null);
+  }
+
+  await priceListStore.fetchPriceList();
+  </script>
